21dccc4da28f4d23294077dcc3b38216f360de9e
Parisa Yalsavar <parisa.yalsavar@gmail.com>
2021-06-05 13:37:11 +0430

    Add london_baseline

--- a5/student-new/src/london_baseline.py
+++ a5/student-new/src/london_baseline.py
@@ -2,3 +2,14 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+import utils
+
+eval_corpus_path = './birth_dev.tsv'
+
+correct = 0
+total = 0
+
+predictions = ['London'] * len(open(eval_corpus_path).readlines())
+total, correct = utils.evaluate_places(eval_corpus_path, predictions)
+# if total > 0:
+print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
